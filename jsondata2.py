caa93dbce7d349e948c74bbf41ece0f7b0a53791
codingwithjon@gmail.com <codingwithjon@gmail.com>
2019-12-29 10:00:43 +1000

    jsondata stuf

---
+++ jsondata2.py
@@ -0,0 +1,23 @@
+import json
+
+jsondata = {
+    "Geeks" : [
+    { 
+      "Geekname" : "Sahil kumar",
+      "subject" : "Data structures",
+      "Articles" : 10
+      },
+    { 
+      "Geekname" : "Pawan singh",
+      "subject" : "Algorithms",
+      "Articles" : 16 
+      },
+    { 
+      "Geekname" : "Ayush Goel",
+      "subject" : "Networking",
+      "Articles" : 7
+      }
+  ]
+}
+
+print(jsondata, index==4)
