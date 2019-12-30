6b7a2ae6ed03188f72544bb1af90b90a72f82c8d
Lynn, Jon <Jon.Lynn@racq.com.au>
2019-12-31 08:48:53 +1000

    updated

--- pigntest.py
+++ pigntest.py
@@ -1,10 +1,10 @@
 import os
 
-ip_list = ["8.8.8.8", "8.8.4.4", "192.168.1.1"]
+ip_list = ["10.51.0.1", "10.51.0.2", "10.51.0.16", "10.51.0.4"]
 
 for ip in ip_list:
     response = os.popen(f"ping {ip}").read()
-    if "Recieved = 4" in response:
+    if "Received = 4" in response:
         print(f"UP {ip} Ping Successful")
     else:
         print(f"DOWN {ip} Ping Unsuccessful")
