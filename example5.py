4e195c569cc4befdab7ac5ddf3e082de186edcf3
codingwithjon@gmail.com <codingwithjon@gmail.com>
2019-12-30 21:04:55 +1000

    this is example 5

---
+++ example5.py
@@ -0,0 +1,28 @@
+print("Hello This Is a String")
+
+stringDATA = ["Hello", "This", "Is a String"]
+for words in stringDATA:
+    print(stringDATA)
+    
+def stringFUNCTION():
+    stringDATA = ["Hello", "This", "Is a String"]
+    for stringDATA in stringDATA:
+        if stringDATA == "Is a String":
+            print("OH MY GOSH ITS A STRING")
+        else:
+            print("STRINGY STRING STRING")
+            
+def integarFUNCTION():
+        integarDATA = 1,2,3,4,5
+        for integarDATA in integarDATA:
+            if integarDATA == 4:
+                print("This number is 4")
+            else:
+                print(integarDATA)
+
+stringFUNCTION()
+integarFUNCTION()
+        
+        
+
+    
